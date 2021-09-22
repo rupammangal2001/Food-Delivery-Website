a28cc508903df470bcfc89314c33ebb60968c037
rupam <rupammangal2001@gmail.com>
2021-09-22 18:26:14 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    alert("Ooppss...! No Food Available Now")
+}
+
+let name =prompt("What is your name?","Castomer");
+console.log(name);
+
+function homeFunction(){
+    
+}
